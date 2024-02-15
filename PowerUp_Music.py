5c72dc0febf6fd2f4cbe47c900b623afefafd788
Smash-Enjoyer <142520767+Smash-Enjoyer@users.noreply.github.com>
2024-02-15 10:38:17 -0500

    Create PowerUp_Music.py

---
+++ PowerUp_Music.py
@@ -0,0 +1,7 @@
+# description: 
+from earsketch import *
+
+setTempo(140)
+
+fitMedia(EIGHT_BIT_ANALOG_DRUM_LOOP_002, 1, 1, 10)
+makeBeat(EIGHT_BIT_ATARI_LEAD_009, 2, 1, "0---------------0---------------0---------------0---------------0+++------------0+++------------0---------------0---------------0")
